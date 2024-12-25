22c7a93042373d656c880fa12fc349771785065a
Fatih <fatihulil@gmail.com>
2024-12-25 21:50:59 +0700

    refactor: :building_construction: Initial setup and chage folder stucture

---
+++ config/siteConfig.ts
@@ -0,0 +1,19 @@
+const config = {
+  title: "FatihUA",
+  description:
+    "A static personal and blog website powered by Next.js and Contentlayer.",
+  author: "Fatih Ulil A",
+  url: "https://fatihua.my.id",
+  socials: {
+    github: "https://github.com/FUA26",
+    linkedin: "https://linkedin.com/id/fatihua",
+    email: "fatihua@gmail.com",
+    site: "https://fatihua.my.id",
+  },
+  blog: {
+    postPerPage: 10,
+    openAllExternalLinksInNewTab: true,
+  },
+};
+
+export default config;
